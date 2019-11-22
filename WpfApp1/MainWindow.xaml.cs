9e4b052306aa89b6e755a514af25d77a50994e6b
rogueXD <vagaming123pro@gmail.com>
2019-11-22 23:34:17 +1030

    messing around with the game atm, nothing new

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CircleCounter = 0;
+        }
+
+        //  the default canvas of the game
+        public Canvas DrawingArea = new Canvas();
+
+        // number of circles clicked
+        private int CircleCounter { get; set; }
+
+        // erase the circle when it's pressed and create another one
+        private void Ellipse_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //Ellipse1.Visibility = Visibility.Collapsed;
+            CircleCounter++;
+            CircleClicked.Text = CircleCounter.ToString();
+        }
+
+    }
+}
