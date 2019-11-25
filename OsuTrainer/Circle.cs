644942964d702a722f40266ce16d674b66a32c2f
rogueXD <vagaming123pro@gmail.com>
2019-11-25 19:29:30 +1030

    continuing the previous commit, but the entire program has been moved to .NET Core instead of .NET Framework. couple circles have been drawn, circle object created and implementing

---
+++ OsuTrainer/Circle.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace OsuTrainer
+{
+    public class Circle
+    {
+        /// <summary>
+        /// * circle constructor, require passing in the size of the drawing area
+        /// * the circleSize difficulty is optional, default to 5
+        /// </summary>
+        /// <param name="area"></param>
+        /// <param name="circleSize"></param>
+        public Circle(ref Grid GameArea)
+        {
+            // setup the default values for the circle
+            // size of the game area
+            int area = (int)GameArea.ColumnDefinitions[2].ActualWidth;
+
+            // circle size
+            CircleSize = 800 / 10;
+
+            // random circle position: x,y
+            Random rand = new Random();
+            X = rand.Next(0, area);
+            Y = rand.Next(0, area);
+
+            // circle number
+            Number = circleNumber++;
+            if (circleNumber > 10)
+            {
+                circleNumber = 0;
+            }
+
+            // circle color
+            CircleColor = Brushes.LightCyan;
+
+
+            // create the ellipses
+            // the inner circle
+            circle = new Ellipse()
+            {
+                Fill = Brushes.Transparent,
+                Stroke = CircleColor,
+                StrokeThickness = CircleSize / 12,
+                Width = CircleSize,
+                Height = CircleSize
+            };
+
+            // the outer ring (initial size)
+            ring = new Ellipse()
+            {
+                Fill = Brushes.Transparent,
+                Stroke = Brushes.White,
+                StrokeThickness = CircleSize / 20,
+                Width = CircleSize * 2,
+                Height = CircleSize * 2
+            };
+        }
+
+        // ellipse object
+        public Ellipse circle { get; private set; }
+        public Ellipse ring { get; private set; }
+
+        // all of the circle attributes
+        // attributes directly affect the difficulty of the game: approach rate and circle size (used for scaling, not actual value)
+        public double ApproachRate { get; private set; }
+        public double CircleSize { get; private set; }
+
+        // position of the circle
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        // visual attribute: circle radius, the outer ring radius, color and number on the circle
+        public Brush CircleColor { get; private set; }
+        public int Number { get; private set; }
+
+        // static variables: approach time, and circle number
+        private static readonly TimeSpan approachRate = TimeSpan.FromMilliseconds(4500);
+        private static int circleNumber = 0;
+
+
+        // functions
+        public ( Ellipse, Ellipse ) DrawCircle()
+        {
+            return ( circle, ring );
+        }
+
+        public void EraseCircle()
+        {
+            circle.Visibility = System.Windows.Visibility.Collapsed;
+            ring.Visibility = System.Windows.Visibility.Collapsed;
+        }
+    }
+}
+
---
+++ OsuTrainer/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Input;
+using System.Windows;
+using System.Windows.Shapes;
+
+namespace OsuTrainer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Combo = 0;
+            Area = (int)gd.ColumnDefinitions[2].ActualWidth;
+
+            // create the first circle
+            Circle circle1 = new Circle(ref gd);
+
+            // get the ellipse drawn from the circle and add it into the canvas
+            // the outer circle must be added in first otherwise it'll overlap the outer circle mouse control
+            (Ellipse, Ellipse) ellipseCircle = circle1.DrawCircle();
+            area.Children.Add(ellipseCircle.Item2);
+            area.Children.Add(ellipseCircle.Item1);
+
+            // subscribe the first circle (innerCircle) to the TapCircle event
+            ellipseCircle.Item1.MouseDown += TapCircle;
+
+        }
+
+        // 2 tapping buttons
+        public char Button1 { get; set; }
+        public char Button2 { get; set; }
+
+        // combo counter
+        public int Combo { get; set; }
+
+        public int Area { get; private set; }
+
+        private void TapCircle(object sender, MouseButtonEventArgs e)
+        {
+            Combo++;
+            ComboText.Text = Combo.ToString() + "x";
+        }
+
+    }
+}
